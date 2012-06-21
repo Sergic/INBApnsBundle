4c70244b302ef3e08cfa03cb7db5d636148a4df5
Sergey Gerdel <skif16@ukr.net>
2012-06-21 16:08:24 +0300

    first commit

---
+++ INBApnsBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the INBApnsBundle package.
+ *
+ * (c) INB Group <http://inbgroup.com/> and Sergey Gerdel <http://sergic.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace INB\ApnsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * @author Sergey Gerdel <skif16@ukr.net>
+ */
+class INBApnsBundle extends Bundle
+{
+}
